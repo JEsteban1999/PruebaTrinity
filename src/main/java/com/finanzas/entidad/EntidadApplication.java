524510fa04f4d9f5f371c073b8db1e8149000413
Juan Esteban Narváez Carvajal <116776212+JEsteban1999@users.noreply.github.com>
2024-09-06 10:55:21 -0500

    Despliegue del proyecto base

---
+++ src/main/java/com/finanzas/entidad/EntidadApplication.java
@@ -0,0 +1,13 @@
+package com.finanzas.entidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadApplication.class, args);
+	}
+
+}
